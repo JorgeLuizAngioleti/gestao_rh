0a36b85fda61c380e85af90b3be6a639d794c82c
JorgeLuizAngioleti <jorgescorpionsound@hotmail.com>
2021-07-23 16:43:54 -0300

    relacionamento_de_bancos

--- apps/funcionarios/models.py
+++ apps/funcionarios/models.py
@@ -1,8 +1,16 @@
 from django.db import models
+#importar user pra funcionarios
+from django.contrib.auth.models import User
+from apps.departamentos.models import Departamento
+from apps.empresas.models import Empresa
+
 
 class Funcionario(models.Model):
     nome = models.CharField(max_length=100)
-
+    # CASCADE se remover user remove funcionario, PROTEC da de deletar um por vez
+    user = models.OneToOneField(User,on_delete=models.PROTECT)
+    departamentos = models.ManyToManyField(Departamento)
+    empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.nome
