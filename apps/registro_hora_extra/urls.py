55f48a9abdfc39a6661c617911c438fa56b7f70b
JorgeLuizAngioleti <jorgescorpionsound@hotmail.com>
2021-07-30 09:54:53 -0300

    atualizando

--- apps/registro_hora_extra/urls.py
+++ apps/registro_hora_extra/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HoraExtraList, HoraExtraEdit, HoraExtraDelete, HoraExtraCreate
+from .views import HoraExtraList, HoraExtraEdit, HoraExtraDelete, HoraExtraCreate, HoraExtraEditVoltarList
 urlpatterns = [
 path('hora_extra', HoraExtraList.as_view(), name='horas_extras'),
 path('editar_hora/<int:pk>/', HoraExtraEdit.as_view(), name='edit_hora_extra'),
+path('editar_hora_list/<int:pk>/', HoraExtraEditVoltarList.as_view(), name='edit_hora_extra_list'),
 path('deletar_hora/<int:pk>/', HoraExtraDelete.as_view(), name='delete_hora_extra'),
 path('criar_hora/', HoraExtraCreate.as_view(), name='create_hora_extra'),
 
--- apps/registro_hora_extra/views.py
+++ apps/registro_hora_extra/views.py
@@ -21,7 +21,7 @@ class HoraExtraList(ListView):
     def get_queryset(self):
         empresa_logada = self.request.user.funcionario.empresa
         return RegistroExtra.objects.filter(funcionario__empresa=empresa_logada)
-
+#edita e volta para funcionarios
 class HoraExtraEdit(UpdateView):
     model = RegistroExtra
     #fields = ['motivo','funcionario','horas']#
@@ -30,9 +30,26 @@ class HoraExtraEdit(UpdateView):
     # metodo para colocar o user no form
     def get_form_kwargs(self):
         kwargs = super(HoraExtraEdit, self).get_form_kwargs()
-        kwargs.update({'user': self.request.user, 'teste': 'alguma coisa'})
+        kwargs.update({'user': self.request.user, 'Titulo': 'Atualizado 1'})
         print(kwargs)
         return kwargs
+
+#edita e volta para list, foi criado duas classes diferentes com duas ulrs(endpoint)
+class HoraExtraEditVoltarList(UpdateView):
+    model = RegistroExtra
+    #fields = ['motivo','funcionario','horas']#
+    form_class = RegistroHoraExtraForm
+    def get_success_url(self):
+        return reverse_lazy('horas_extras')
+        #return reverse_lazy('edit_hora_extra_list',args=[self.object.id])
+
+    # metodo para colocar o user no form
+    def get_form_kwargs(self):
+        kwargs = super(HoraExtraEditVoltarList, self).get_form_kwargs()
+        kwargs.update({'user': self.request.user, 'teste': 'Atualizado 2'})
+        print(kwargs)
+        return kwargs
+
 
 class HoraExtraDelete(DeleteView):
     model = RegistroExtra
